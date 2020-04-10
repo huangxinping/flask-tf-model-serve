5b4167f91484b4786483e7588254f5919424cd76
huangxinping <o0402@icloud.com>
2020-04-10 12:14:36 +0800

    Update entrypoints.sh

--- entrypoints.sh
+++ entrypoints.sh
@@ -1,2 +1,2 @@
-nohup tensorflow_model_server --model_base_path=/app/mobile_v2 --rest_api_port=9000 --model_name=ImageClassifier --enable_batching=true &
-export FLASK_ENV=development && flask run --host=0.0.0.0
+tensorflow_model_server --model_base_path=/app/mobile_v2 --rest_api_port=9000 --model_name=ImageClassifier --enable_batching=true
+nohup export FLASK_ENV=development && flask run --host=0.0.0.0 &
