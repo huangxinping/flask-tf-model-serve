22ba8cd52416b789e97012bac8133de703d38c0a
huangxinping <huangxp@kexinbao100.com>
2020-04-10 10:11:09 +0800

    fix entry file.

--- entrypoints.sh
+++ entrypoints.sh
@@ -1,3 +1,3 @@
 nohup flask run --host=0.0.0.0 &
 
-tensorflow_model_server --model_base_path=mobile_v2 --rest_api_port=9000 --model_name=ImageClassifier
+tensorflow_model_server --model_base_path=/app/mobile_v2 --rest_api_port=9000 --model_name=ImageClassifier
