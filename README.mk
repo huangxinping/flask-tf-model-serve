c5cb7ab6ab047ca9c66c5b6e492f4f076a6124d1
huangxinping <huangxp@kexinbao100.com>
2020-04-10 09:47:58 +0800

    update README.md

--- README.mk
+++
@@ -1,4 +0,0 @@
- # Tensorflow model serve with flask.
-
- ## docker build -t huangxinping/flask-tf-model-serve .
- ## docker run -d -p 5000:5000 --name flask-tf-model-serve huangxinping/flask-tf-model-serve
