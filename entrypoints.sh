1847787b3947df404264754a2d345889e1b605f8
huangxinping <huangxp@kexinbao100.com>
2020-04-10 10:25:25 +0800

    fix flask serve type.

--- entrypoints.sh
+++ entrypoints.sh
@@ -1,3 +1,4 @@
 nohup tensorflow_model_server --model_base_path=/app/mobile_v2 --rest_api_port=9000 --model_name=ImageClassifier &
 
-flask run --host=0.0.0.0
+# flask run --host=0.0.0.0
+python app.py
